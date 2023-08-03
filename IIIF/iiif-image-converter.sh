6bccb51445434f1737eadad66611a41bca5d5c3c
AITProjectAssistant <admin@ait.co.at>
2023-08-03 08:37:01 +0200

    Update iiif-image-converter.sh
    Changed the compression in the image transformation to reduce the image size. Added log file generation.

--- IIIF/iiif-image-converter.sh
+++ IIIF/iiif-image-converter.sh
@@ -24,6 +24,8 @@ if ! command_exists vips ; then
 	install_vips;
 fi
 
+LOG=`date +%Y%m%d_%H%M%S`"_imgconverted.log"
+I=1
 # Convert all images in a directory to tiff
 find . -not -name "*.sh" -not -name "*.tif" -type f | while read file
 do
@@ -31,8 +33,15 @@ do
     echo "File ${file%.*}.tif already exists. Skipping."
     continue
   fi
-  echo "Converting $file to ${file%.*}.tif"  
-  vips im_vips2tiff "$file" "${file%.*}".tif:deflate,tile:256x256,pyramid;
+  echo "*$I: Converting $file to ${file%.*}.tif" >> $LOG
+  # "deflate" - ZIP (deflate) compression
+  # vips im_vips2tiff "$file" "${file%.*}".tif:deflate,tile:256x256,pyramid;
+  # "jpeg" - JPEG compression :## JPEG quality level;
+  vips im_vips2tiff "$file" "${file%.*}".tif:jpeg:90,tile:256x256,pyramid;
+  exec 1>>$LOG 2>>$LOG
+  echo "=== Done ===" >> $LOG
+  I=$((I+1))
+  
 done
 
 # Move all tiff files to a new directory
